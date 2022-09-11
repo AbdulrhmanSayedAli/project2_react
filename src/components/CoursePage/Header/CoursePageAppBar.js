286ca288a35f211615828bac5e2df7f1790d2a9f
AbdulrhmanSayedAli <roctes7@gmail.com>
2022-09-12 00:34:32 +0300

    Adding responsive bottom bar in small screens

--- src/components/CoursePage/Header/CoursePageAppBar.js
+++ src/components/CoursePage/Header/CoursePageAppBar.js
@@ -7,12 +7,21 @@ export default function CoursePageAppBar() {
   const course = useContext(SimpleCourseContext);
   return (
     <div className="app-bar bg-dark">
-      <div className="p-2 content ">
-        <h1 className="ms-lg-5 my-2">
-          <strong>{course.title}</strong>
-        </h1>
-        <HeaderRatingBar />
+      <div className="p-2 d-flex align-items-center content bg-dark">
+        <div className=" flex-grow-1 inner">
+          <h1 className="ms-lg-5 my-2">
+            <strong>{course.title}</strong>
+          </h1>
+          <HeaderRatingBar />
+        </div>
+        <h3 className="mx-3  d-lg-none">
+          <strong>{course.newPrice}$</strong>
+        </h3>
+        <button className="mx-3 d-lg-none">
+          <strong>Buy now</strong>
+        </button>
       </div>
+
       <HeaderTabs />
     </div>
   );
